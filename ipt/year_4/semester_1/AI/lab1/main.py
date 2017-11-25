4ff7faa04314baec6bd9c49864426571a7da7c74
Nikita-L <lyovkin@i.ua>
2017-11-26 01:57:50 +0200

    Code refactor

--- ipt/year_4/semester_1/AI/lab1/main.py
+++ ipt/year_4/semester_1/AI/lab1/main.py
@@ -13,7 +13,7 @@ def main():
     err = 0
 
     for i in range(0, 50000):
-        k = random.randint(0, 80)
+        k = random.randint(0, 79)
         y = w1 * my_data[k][0] + w2 * my_data[k][1] + w0
         if y < 0:
             y = 0
@@ -25,7 +25,7 @@ def main():
             w2 = w2 + n * (my_data[k][2] - y) * my_data[k][1]
             w0 = w0 + n * (my_data[k][2] - y) * 1
 
-    for j in range(81, 100):
+    for j in range(80, 100):
         y = w1 * my_data[j][0] + w2 * my_data[j][1] + w0
         if y < 0:
             y = 0
@@ -39,13 +39,13 @@ def main():
     zerox1_x2 = np.empty((20, 2))
     onex1_x2 = np.empty((20, 2))
 
-    for k in range(0, 19):
-        if my_data[k + 81, 2] == 0:
-            zerox1_x2[k, 0] = my_data[k + 81, 0]
-            zerox1_x2[k, 1] = my_data[k + 81, 1]
-        elif my_data[k + 81, 2] == 1:
-            onex1_x2[k, 0] = my_data[k + 81, 0]
-            onex1_x2[k, 1] = my_data[k + 81, 1]
+    for k in range(0, 20):
+        if my_data[k + 80, 2] == 0:
+            zerox1_x2[k, 0] = my_data[k + 80, 0]
+            zerox1_x2[k, 1] = my_data[k + 80, 1]
+        elif my_data[k + 80, 2] == 1:
+            onex1_x2[k, 0] = my_data[k + 80, 0]
+            onex1_x2[k, 1] = my_data[k + 80, 1]
 
     plt.plot(zerox1_x2[:, 0], zerox1_x2[:, 1], 'ro', onex1_x2[:, 0],
              onex1_x2[:, 1], 'bo', t, (-w1 * t - w0) / w2, 'k')
@@ -55,9 +55,9 @@ def main():
     plt.show()
 
     print(f"Errors: {err}")
+    print(f"w0: {w0}")
     print(f"w1: {w1}")
     print(f"w2: {w2}")
-    print(f"w0: {w0}")
 
 
 if __name__ == "__main__":
